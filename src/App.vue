c2544a63f809249da24ba6bcff2207ad1c8392f5
zioan <zaharia.ioan@gmail.com>
2021-11-06 17:14:04 +0100

    list animation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <button type="button" @click="flag = !flag">Toggle</button>
-
-  <transition @enter="enter" @leave="leave" :css="false">
-    <h2 v-if="flag">Hello</h2>
-  </transition>
+  <button @click="addItem">Add</button>
+  <ul>
+    <transition-group name="fade">
+      <li
+        v-for="(number, index) in numbers"
+        :key="number"
+        @click="removeItem(index)"
+      >
+        {{ number }}
+      </li>
+    </transition-group>
+  </ul>
 </template>
 
 <script>
@@ -11,29 +18,42 @@ export default {
   name: "App",
   data() {
     return {
-      flag: false,
+      numbers: [1, 2, 3, 4, 5],
     };
   },
   methods: {
-    enter(el, done) {
-      const animation = el.animate([{ transform: "scale3d(0,0,0)" }, {}], {
-        duration: 1000,
-      });
-
-      animation.onfinish = () => {
-        done();
-      };
+    addItem() {
+      const num = Math.floor(Math.random() * 100 + 1);
+      const index = Math.floor(Math.random() * this.numbers.length);
+      this.numbers.splice(index, 0, num);
     },
-
-    leave(el, done) {
-      const animation = el.animate([{}, { transform: "scale3d(0,0,0)" }], {
-        duration: 1000,
-      });
-
-      animation.onfinish = () => {
-        done();
-      };
+    removeItem(index) {
+      this.numbers.splice(index, 1);
     },
   },
 };
 </script>
+
+<style>
+li {
+  font-size: 20px;
+  cursor: pointer;
+}
+
+.fade-move {
+  transition: all 0.5s linear;
+}
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: all 1s linear;
+}
+.fade-leave-to {
+  transition: all 1s linear;
+  opacity: 0;
+}
+.fade-leave-active {
+  position: absolute;
+}
+</style>
