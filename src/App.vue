5d997898707c7b0d143a051aa2091b12060fe34c
zioan <zaharia.ioan@gmail.com>
2021-11-06 16:13:48 +0100

    css animation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <button type="button" @click="flag = !flag">Toggle</button>
 
-  <transition name="fade" mode="out-in">
+  <!-- <transition name="fade" mode="out-in">
     <h1 v-if="flag" key="main">Hello World</h1>
     <h1 v-else key="secondary">Another Hello</h1>
+  </transition> -->
+  <transition name="zoom" appear>
+    <h2 v-if="flag">Hello</h2>
   </transition>
 </template>
 
@@ -12,13 +15,18 @@ export default {
   name: "App",
   data() {
     return {
-      flag: false,
+      flag: true,
     };
   },
 };
 </script>
 
 <style>
+h2 {
+  widows: 400px;
+  padding: 20px;
+  margin: 20px;
+}
 .fade-enter-from {
   opacity: 0;
 }
@@ -28,5 +36,32 @@ export default {
 .fade-leave-to {
   transition: all 0.5s linear;
   opacity: 0;
+}
+
+/* .zoom-enter-from {
+    opacity: 0;
+  } */
+.zoom-enter-active {
+  animation: zoom-in 1s linear forwards;
+}
+.zoom-leave-to {
+  animation: zoom-out 1s linear forwards;
+}
+
+@keyframes zoom-in {
+  from {
+    transform: scale(0, 0);
+  }
+  to {
+    transform: scale(1, 1);
+  }
+}
+@keyframes zoom-out {
+  from {
+    transform: scale(1, 1);
+  }
+  to {
+    transform: scale(0, 0);
+  }
 }
 </style>
